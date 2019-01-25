5897e3b7ac4e014a27f66ca85ef9c99ffa0955b5
Laura Ruiz-Roehrs <lauraruizroehrs@gmail.com>
2019-01-25 16:38:31 -0600

    added jQuery & first fetch function

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-alert("js connected");
+function listOfAllYogaPoses(){
+  fetch('https://raw.githubusercontent.com/rebeccaestes/yoga_api/master/yoga_api.json')
+  .then(result => result.json())
+  .then(newResult => console.log(newResult))
+  .catch(error => console.log(error))
+}
+
+function init() {
+	listOfAllYogaPoses();
+}
+
+$(init); //document on ready
