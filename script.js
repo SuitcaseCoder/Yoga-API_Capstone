f55ed2569cd898fdc3a47e804ee68f661d63a99e
Laura Ruiz-Roehrs <lauraruizroehrs@gmail.com>
2019-01-26 00:32:08 -0600

    value from selected option

--- script.js
+++ script.js
@@ -8,6 +8,16 @@ function listOfAllYogaPoses(){
     console.log(error))
 }
 
+function fetchYogaApiAgain(){
+  fetch('https://raw.githubusercontent.com/rebeccaestes/yoga_api/master/yoga_api.json')
+  .then(result =>
+    result.json())
+  .then(newResult =>
+    getVariableforAllPoses(newResult))
+  .catch(error =>
+    console.log(error))
+}
+
 function getVariableforAllPoses(newResult){
   for(let i=0; i<newResult.length; i++){
     let allPoses = newResult[i].english_name;
@@ -23,6 +33,19 @@ function getVariableforAllPoses(newResult){
 }
 }
 
+function submitButton(){
+  $('.dropdown-form').submit(event => {
+    event.preventDefault();
+
+    let valueSelected = $('.dropdown-style:selected').val();
+    console.log(valueSelected);
+
+    //loop through the object using valueSelected ?
+
+})
+}
+
+
 
 
 
@@ -37,6 +60,7 @@ function getVariableforAllPoses(newResult){
 
 function init() {
 	listOfAllYogaPoses();
+  submitButton();
 }
 
 $(init); //document on ready
