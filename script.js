944cb19066f62bf9fba61949ff5c9d4dcc740dbd
Laura Ruiz-Roehrs <lauraruizroehrs@gmail.com>
2019-01-28 13:46:42 -0600

    looped through object sel selected to access sanskritname and img url

--- script.js
+++ script.js
@@ -2,23 +2,17 @@ function listOfAllYogaPoses(){
   fetch('https://raw.githubusercontent.com/rebeccaestes/yoga_api/master/yoga_api.json')
   .then(result =>
     result.json())
-  .then(newResult =>
-    getVariableforAllPoses(newResult))
+  .then(newResult => {
+    getVariableforAllPoses(newResult)
+    var newResult = newResult;
+  })
   .catch(error =>
     console.log(error))
 }
 
-function fetchYogaApiAgain(){
-  fetch('https://raw.githubusercontent.com/rebeccaestes/yoga_api/master/yoga_api.json')
-  .then(result =>
-    result.json())
-  .then(newResult =>
-    getVariableforAllPoses(newResult))
-  .catch(error =>
-    console.log(error))
-}
 
 function getVariableforAllPoses(newResult,valueSelected){
+  console.log(newResult);
   for(let i=0; i<newResult.length; i++){
     let allPoses = newResult[i].english_name;
     console.log(allPoses);
@@ -45,29 +39,34 @@ function getVariableforAllPoses(newResult,valueSelected){
     }
 
 
-}
+  }
+  submitButton(newResult);
 }
 
-function submitButton(newResult, sanskritName, poseImage){
+function submitButton(newResult){
   $('.dropdown-form').submit(event => {
     event.preventDefault();
-
     let valueSelected = $('.dropdown-style:selected').val();
-    alert(valueSelected);
 
-    $('.results').empty();
-    $('.results').append(`
-        <h2>Sanskrit Name</h2>
-        <p>${sanskritName}</p>
-        <img src="${poseImage}"></img>
+    for(let i=0; i<newResult.length; i++){
+      if(valueSelected === newResult[i].english_name){
+        let objectSelected = newResult[i];
+        console.log(newResult[i]);
+        displayResults(objectSelected);
+      }
+    }
 
-      `)
-    console.log(sanskritName);
-
-
-})
+  });
 }
 
+function displayResults(objectSelected){
+  $('.results').empty();
+  $('.results').append(`
+      <h2>Sanskrit Name</h2>
+      <p>${objectSelected.sanskrit_name}</p>
+      <img src="${objectSelected.img_url}"></img>
+    `)
+}
 
 
 //how do I make either valueSelected accessible in all functions
@@ -76,16 +75,8 @@ function submitButton(newResult, sanskritName, poseImage){
 
 
 
-
-
-
-
-
-
-
 function init() {
 	listOfAllYogaPoses();
-  submitButton();
 }
 
 $(init); //document on ready
